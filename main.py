2205057cedb33bfca41ff977a3b00d2ed5f8f09d
terrentii <terrentii@vk.com>
2025-11-27 22:34:08 +0300

    Обучение
    Познавание основ arcade

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import arcade
+
+WIDTH = 1280
+HEIGHT = 720
+TITLE = 'Owl`s last scream'
+
+
+class Window(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.AMAZON) # Загрузка фона (зелёный цвет)
+
+    def on_draw(self):
+        """Здесь живут спрайты"""
+        self.clear()
+        arcade.draw_circle_filled(400, 300, 50, arcade.color.RED) #Просто круг
+
+    def update(self, delta_time):
+        pass
+
+
+game = Window(WIDTH, HEIGHT, TITLE)
+arcade.run()
