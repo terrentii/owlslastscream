72c0c2c0f6d084e93ce24285a6c5c2b4716b5651
terrentii <terrentii@vk.com>
2025-11-29 08:14:15 +0300

    Добавлена фигурка

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ TITLE = 'Owl`s last scream'
 class Window(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.AMAZON) # Загрузка фона (зелёный цвет)
+        arcade.set_background_color(arcade.color.DARK_GREEN) # Загрузка фона (зелёный цвет)
+
+        self.alien_list = arcade.SpriteList()
+        self.alien = arcade.Sprite('resources/persons/alien_in_pukhovik_new_year.png', 1.5)
+
+        #Позиционирование
+        self.alien.center_y = HEIGHT // 2
+        self.alien.center_x = WIDTH // 2
+
+        #Добавление в листы
+        self.alien_list.append(self.alien)
 
     def on_draw(self):
         """Здесь живут спрайты"""
         self.clear()
-        arcade.draw_circle_filled(400, 300, 50, arcade.color.RED) #Просто круг
+        self.alien_list.draw() #Зелёный человечек
 
     def update(self, delta_time):
+        self.alien.update()
         pass
 
 
