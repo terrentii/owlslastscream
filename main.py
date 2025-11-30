391acd2ac41627fa2e1ad34539baed659042f84b
terrentii <terrentii@vk.com>
2025-11-30 11:55:02 +0300

    Вращение про ходьбе влево

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ class RunningAlien(arcade.Sprite):
         self.textures = []
 
         # Бег
+        self.running_textures_right = []
+        self.running_textures_left = []
         for i in range(14):
             frame_number = f"{i:02d}"
-            alien_run = arcade.load_texture(f'resources/persons/alien_running/sprite_{frame_number}.png')
-            self.textures.append(alien_run)
+            # движение вправо
+            alien_run_right = arcade.load_texture(f'resources/persons/alien_running/sprite_{frame_number}.png')
+            self.running_textures_right.append(alien_run_right)
+            # движение влево
+            alien_run_left = arcade.load_texture(f'resources/persons/alien_running/sprite_{frame_number}.png')
+            alien_run_left = alien_run_left.flip_left_right()
+            self.running_textures_left.append(alien_run_left)
 
-        self.idle_texture = arcade.load_texture('resources/persons/steady_alien.png')
+        # idle
+        self.idle_texture_right = arcade.load_texture('resources/persons/steady_alien.png')
+        self.idle_texture_left = arcade.load_texture('resources/persons/steady_alien.png')
+        self.idle_texture_left = self.idle_texture_left.flip_left_right()
 
-        self.texture = self.textures[0]
+        self.texture = self.running_textures_right[0]
 
         # Для анимации
         self.current_frame = 0
@@ -28,19 +38,24 @@ class RunningAlien(arcade.Sprite):
         self.frames_per_second = 24
 
     def update_animation(self, delta_time):
-        # Анимация покоя
+        # Анимация
         if abs(self.change_x) > 0:
             # Бежит
             self.time_since_last_frame += delta_time
             frame_duration = 1.0 / self.frames_per_second
 
             if self.time_since_last_frame >= frame_duration:
-                self.current_frame = (self.current_frame + 1) % len(self.textures)
-                self.texture = self.textures[self.current_frame]
+                self.current_frame = (self.current_frame + 1) % len(self.running_textures_right)
+                # направление
+                if self.change_x > 0:
+                    self.texture = self.running_textures_right[self.current_frame]
+                else:
+                    self.texture = self.running_textures_left[self.current_frame]
                 self.time_since_last_frame = 0
         else:
             # Стоит
-            self.texture = self.idle_texture
+            if self.change_x == 0:
+                self.texture = self.idle_texture_right if self.change_x >= 0 else self.idle_texture_left
             self.current_frame = 0
             self.time_since_last_frame = 0
 
