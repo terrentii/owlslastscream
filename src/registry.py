d0e45980e65670e5c9cad5bb6ea2b9b716657536
terrentii <terrentii@vk.com>
2025-12-14 20:21:06 +0300

    Картинки

--- src/registry.py
+++ src/registry.py
@@ -4,8 +4,13 @@ ANIMATION_FRAME_DURATION = 1.0 / 24  # 24 кадра в секунду
 # Пути к ресурсам
 BACKGROUND_PATH = 'resources/background/image.png'
 ALIEN_RUNNING_PATH = 'resources/persons/alien_running/sprite_'
-ALIEN_IDLE_RIGHT_PATH = 'resources/persons/steady_alien(1).png'
-ALIEN_IDLE_LEFT_PATH = 'resources/persons/steady_alien(2).png'
+ALIEN_IDLE_SPACESUIT_RIGHT_PATH = 'resources/persons/steady_alien_spacesuit_right.png'
+ALIEN_IDLE_SPACESUIT_LEFT_PATH = 'resources/persons/steady_alien_spacesuit_left.png'
+# ALIEN_IDLE_RIGHT_PATH = 'resources/persons/steady_alien_right.png'
+ALIEN_IDLE_LEFT_PATH = 'resources/persons/steady_alien_left.png'
+ALIEN_IDLE_RIGHT_PATH = 'resources/persons/steady_alien_spacesuit_right.png'
+# ALIEN_IDLE_LEFT_PATH = 'resources/persons/steady_alien_spacesuit_left.png'
+
 
 # Костыль
 reg = None
