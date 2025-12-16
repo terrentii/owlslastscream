14a83c74e9f97d458567e0b1e292c587d5ea52c8
terrentii <terrentii@vk.com>
2025-12-16 17:02:30 +0300

    Openworld update

--- src/windows/game_view.py
+++ src/windows/game_view.py
@@ -1,4 +1,5 @@
 import arcade
+import random as rd
 from src.settings import settings
 from src.animations.RunningAlien import RunningAlien
 
@@ -15,8 +16,18 @@ class GameView(arcade.View):
         self.bg_list = arcade.SpriteList()
         self.bg_list.append(self.bg)
         
+        # Настройка елок
+        self.spruce_list = arcade.SpriteList()
+        for i in range(1000):
+            spruce = arcade.Sprite('resources/background/outer_space/spruce_1.png', scale=0.5)
+            spruce.center_x = rd.randint(-10000, 10000)
+            spruce.center_y = rd.randint(-10000, 10000)
+            self.spruce_list.append(spruce)
+        
         # Настройка камеры
         self.camera = arcade.camera.Camera2D()
+        self.camera.use()
+        self.camera.zoom = 0.75
         
         # Настройка пришельца
         self.alien_list = arcade.SpriteList()
@@ -26,13 +37,13 @@ class GameView(arcade.View):
         self.alien.center_y = settings.height // 4
         self.alien_list.append(self.alien)
         
-        # Настройка персонажа Ness
+        # Настройка Ness
         self.ness = arcade.Sprite('resources/persons/alien_ness/ness_no_anim.png', scale=0.22)
-        # Размещаем Ness в случайном месте поблизости от центра
+        # Ness
         import random
         center_x = settings.width // 2 + 50
         center_y = settings.height // 4
-        # Случайное смещение от центра в пределах ±200 пикселей
+        
         self.ness.center_x = center_x + random.randint(-200, 200)
         self.ness.center_y = center_y + random.randint(-200, 200)
         self.alien_list.append(self.ness)
@@ -47,36 +58,34 @@ class GameView(arcade.View):
         """Отрисовка игрового экрана"""
         self.clear()
         
-        # Применение камеры
         self.camera.use()
         self.bg_list.draw()
+        self.spruce_list.draw()
         self.alien_list.draw()
 
     def on_update(self, delta_time):
         """Обновление игровой логики"""
-        # Управление скоростью по оси X
+        # скорость по оси X
         self.alien.change_x = 0
         if self.left_pressed and not self.right_pressed:
             self.alien.change_x = -settings.PLAYER_SPEED
         elif self.right_pressed and not self.left_pressed:
             self.alien.change_x = settings.PLAYER_SPEED
             
-        # Управление скоростью по оси Y
+        # скорость по оси Y
         self.alien.change_y = 0
         if self.up_pressed and not self.down_pressed:
             self.alien.change_y = settings.PLAYER_SPEED
         elif self.down_pressed and not self.up_pressed:
             self.alien.change_y = -settings.PLAYER_SPEED
             
-        # Обновление спрайтов
         self.alien_list.update()
         self.alien.update_animation(delta_time)
-        
-        # Убраны ограничения движения - пришелец может выходить за края экрана
-        # Ограничения были удалены по запросу пользователя
-            
-        # Обновление камеры, чтобы следовать за пришельцем по обеим осям
-        self.camera.position = self.alien.center_x, self.alien.center_y
+
+        # Позиция камеры с небольшим смещением вверх
+        camera_x = self.alien.center_x
+        camera_y = self.alien.center_y + settings.height * 0.2  # Поднимаем камеру на половину высоты экрана
+        self.camera.position = camera_x, camera_y
 
     def on_key_press(self, key, modifiers):
         """Обработка нажатия клавиш"""
