b04e570b022867df73798b00b5aabdc4a15f7445
terrentii <terrentii@vk.com>
2026-01-10 11:46:17 +0300

    Персонаж

--- src/registry.py
+++ src/registry.py
@@ -4,13 +4,11 @@ ANIMATION_FRAME_DURATION = 1.0 / 24  # 24 кадра в секунду
 # Пути к ресурсам
 BACKGROUND_PATH = 'resources/background/snow_blank.png'
 ALIEN_RUNNING_PATH = 'resources/persons/alien_running/sprite_'
+ALIEN_RUNNING_IN_SPACESUIT_PATH = 'resources/persons/alien_in_spacesuit/steady_alien_spacesuit_right-export'
 ALIEN_IDLE_SPACESUIT_RIGHT_PATH = 'resources/persons/steady_alien_spacesuit_right.png'
 ALIEN_IDLE_SPACESUIT_LEFT_PATH = 'resources/persons/steady_alien_spacesuit_left.png'
-# ALIEN_IDLE_RIGHT_PATH = 'resources/persons/steady_alien_right.png'
-# ALIEN_IDLE_LEFT_PATH = 'resources/persons/steady_alien_left.png'
 ALIEN_IDLE_RIGHT_PATH = 'resources/persons/steady_alien_spacesuit_right.png'
 ALIEN_IDLE_LEFT_PATH = 'resources/persons/steady_alien_spacesuit_left.png'
-# ALIEN_RUN_GIF_PATH = 'resources/persons/alien_running/running_alien_right.gif'
 
 # Костыль
 reg = None
